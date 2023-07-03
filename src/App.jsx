ae4ff437bee5a4294e0f68b85ac1c336f495b6fc
Mirhasankhan <mirhasan000034@gmail.com>
2023-07-03 21:16:00 +0600

    project setup done

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,37 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import profile from '../src/assets/profile.jpg'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="drawer lg:drawer-open">
+        <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
+        <div className="drawer-content bg-[#001E3C] text-white ml-1">
+          <div id='home' className='flex justify-start items-center mx-6'>
+            <div className='w-1/2'>
+              <h1>Welcome To My World</h1>
+              <h1>Hi,I'm Mir Hasan</h1>
+              <h3>I am a MERN Stack Developer</h3>
+              <p>Hey,I'm Rakib. I am an Junior Frontend Developer.I am a website developer from Bangladesh. I create professional websites. As a web developer, I have a passion for creating dynamic and engaging online experiences.</p>
+            </div>
+            <img className='h-64 w-64 rounded-full ml-6' src={profile} alt="" />
+          </div>
+          <label htmlFor="my-drawer-2" className="btn btn-primary drawer-button lg:hidden">Open drawer</label>
+        </div>
+        <div className="drawer-side">
+          <label htmlFor="my-drawer-2" className="drawer-overlay"></label>
+          <ul className="menu p-4 w-64 h-full bg-[#001E3C] text-white">
+            {/* Sidebar content here */}
+            <li><a href='#home'>Home</a></li>
+            <li><a href='#about'>About</a></li>
+            <li><a href='#service'>Service</a></li>
+            <li><a href='#skill'>Skill</a></li>
+            <li><a href='#contact'>Contact</a></li>
+          </ul>
+
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
